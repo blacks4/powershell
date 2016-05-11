b27d964483419a6cbb05a7544678e0e57186b7bd
Steve Tractenberg <steve@blacks4.com>
2016-05-11 08:27:58 -0400

    new file

---
+++ rename-host-to-EC2-Name-Tag.ps1
@@ -0,0 +1,6 @@
+﻿$InstanceId = (Invoke-RestMethod 'http://169.254.169.254/latest/meta-data/instance-id').ToString()
+$AvailabilityZone = (Invoke-RestMethod 'http://169.254.169.254/latest/meta-data/placement/availability-zone').ToString()
+$Region = $AvailabilityZone.Substring(0,$AvailabilityZone.Length-1)
+$Tags = Get-EC2Tag -Filters @{Name='resource-id';Value=$InstanceId}  -Region $Region -AccessKey XXXXXXXXXXXXXX -SecretKey XXXXXXXXXXXXXXXXXXXXX
+$InstanceName = ($Tags | Where-Object {$_.Key -eq 'Name'}).Value
+Rename-Computer -NewName $InstanceName -Force
